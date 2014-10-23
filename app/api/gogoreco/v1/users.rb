1a3f8194ef0f2fbd8b6ca4347cc2a6a7068fbe46
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-23 17:27:11 +0200

    should like, v1

--- app/api/gogoreco/v1/users.rb
+++ app/api/gogoreco/v1/users.rb
@@ -42,6 +42,17 @@ module Gogoreco
           #end
           #}}}
 
+          #{{{ should_like_items
+          desc "get the items I should like"
+          params do 
+            optional :nmax, type: Integer, desc: "max number of items(default10)", default: 10
+          end
+          post :should_like_items do
+            check_confirmed_user!
+            present items: current_user.should_like, with: Gogoreco::Entities::Item, entity_options: entity_options
+          end
+          #}}}
+
 
         end
       end
