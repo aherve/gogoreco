8b99ddda083c839dbe3e38d22d6fd9fe6329c097
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-24 18:27:17 +0200

    typo

--- app/api/gogoreco/entities/comment.rb
+++ app/api/gogoreco/entities/comment.rb
@@ -4,7 +4,8 @@ module Gogoreco
       expose :pretty_id, as: :id
       expose :content, if: lambda {|s,o| o[:entity_options]["comment"][:content]}
       expose :related_evaluation, using: Gogoreco::Entities::Evaluation, if: lambda{|s,o| o[:entity_options]["comment"][:related_evaluation]}
-      expose :author, using: Gogoreco::Entities::User, if: lambda{|s,o|, o[:entity_options]["comment"][:author]}
+
+      expose :author, using: Gogoreco::Entities::User, if: lambda{|s,o| o[:entity_options]["comment"][:author]}
     end
   end
 end
