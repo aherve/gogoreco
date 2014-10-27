b17a3648b79484ac5a4604d6e485741763ca2419
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-27 17:05:39 +0100

    after-confirm redirect

--- app/controllers/devise/confirmations_controller.rb
+++ app/controllers/devise/confirmations_controller.rb
@@ -42,10 +42,11 @@ class Devise::ConfirmationsController < DeviseController
 
   # The path used after confirmation.
   def after_confirmation_path_for(resource_name, resource)
-    if signed_in?
-      signed_in_root_path(resource)
-    else
-      new_session_path(resource_name)
-    end
+    FRONT_APP_ROOT_URL
+    #if signed_in?
+    #  signed_in_root_path(resource)
+    #else
+    #  new_session_path(resource_name)
+    #end
   end
 end
