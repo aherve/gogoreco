c16b0e3ff77b43295ef590b7b73fb64fe15fd011
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-27 16:31:06 +0100

    root url for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ GogorecoApi::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'gogoreco.com'}
+  config.action_mailer.default_url_options = { :host => 'gogoreco.io'}
   config.action_mailer.delivery_method = :ses
 
 end
